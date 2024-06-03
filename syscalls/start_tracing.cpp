e6eab960fb328fab8321d33f0eb9cd0e7e50267e
Tanya Prasad <tanyapsd@students.cs.ubc.ca>
2024-06-03 14:53:58 -0700

    added example sys call

---
+++ syscalls/start_tracing.cpp
@@ -0,0 +1,25 @@
+/*
+ * Test the (#451)
+ */
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <stdio.h>
+
+/*
+ * Put your syscall number here.
+ */
+#define SYS_three_start_tracing 451
+
+int main(int argc, char **argv)
+{
+  if (argc <= 1) {
+    printf("Must provide a string to give to system call.\n");
+    return -1;
+  }
+  char *arg = argv[1];
+  printf("Making system call with \"%s\".\n", arg);
+  long res = syscall(SYS_three_start_tracing, arg);
+  printf("System call returned %ld.\n", res);
+  return res;
+}
